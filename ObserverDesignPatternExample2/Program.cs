d389dabd1ba409454e80addcb374631df7b8eeae
naimolcer <naimolcer@gmail.com>
2024-04-18 21:31:40 +0300

    Add project files.

---
+++ ObserverDesignPatternExample2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObserverDesignPatternExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            
+            subject.SomeBusinessLogic();
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ObserverDesignPatternExample2/Subject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPatternExample2
+{
+    public class Subject : ISubject
+    {
+        public int State { get; set; }
+
+        private List<IObserver> _observers = new List<IObserver>();
+        public void Attach(IObserver observer)
+        {
+            Console.WriteLine("Subject : Attached an observer");
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            Console.WriteLine("Subject : Detached an observer");
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine("Notifying Observers...");
+
+            foreach (var item in _observers)
+            {
+                item.Update(this);
+            }
+        }
+        public void SomeBusinessLogic()
+        {
+            this.State = new Random().Next(0, 10);
+
+            Thread.Sleep(15);
+
+            Console.WriteLine("State is changed");
+
+            this.Notify();
+        }
+    }
+}
